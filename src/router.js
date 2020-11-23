35400b654d3a776f32a0a506567da28d78e0210d
Edwin Garcia <spark.com.co@gmail.com>
2020-11-23 18:20:45 -0500

    Protegiendo las rutas utilizando navigation guards

--- src/router.js
+++ src/router.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+
+import store from './store';
 import HomePage from './views/HomePage.vue';
 import SearchPage from './views/SearchPage.vue';
 import NotFoundPage from './views/NotFoundPage.vue';
@@ -11,7 +13,7 @@ import HousesPages from './views/user/HousesPage.vue';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -33,11 +35,17 @@ export default new Router({
       path: '/user/profile',
       name: 'ProfilePage',
       component: ProfilePage,
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
       path: '/user/houses',
       name: 'HousesPages',
       component: HousesPages,
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
       path: '/house',
@@ -47,6 +55,9 @@ export default new Router({
       path: '/house/new',
       name: 'CreateHousePage',
       component: CreateHousePage,
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
       path: '*',
@@ -55,3 +66,17 @@ export default new Router({
     },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(route => route.meta.requiresAuth)) {
+    if (store.state.authId) {
+      next();
+    } else {
+      next({ name: 'HomePage' });
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
