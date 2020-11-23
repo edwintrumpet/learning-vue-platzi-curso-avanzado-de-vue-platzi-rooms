28916f905096d84e31ffb755d863c5295b02ae7e
Edwin Garcia <spark.com.co@gmail.com>
2020-11-23 07:27:51 -0500

    Instalacion y configuracion de Firebase realtime database

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 import Vue from 'vue';
+import firebase from 'firebase/app';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyACLGuyj7kofNgmA5yIOPs97aO4To7jR-Q',
+  authDomain: 'platzi-rooms-cursoavanzadovue.firebaseapp.com',
+  databaseURL: 'https://platzi-rooms-cursoavanzadovue.firebaseio.com',
+  projectId: 'platzi-rooms-cursoavanzadovue',
+  storageBucket: 'platzi-rooms-cursoavanzadovue.appspot.com',
+  messagingSenderId: '360280269690',
+  appId: '1:360280269690:web:f2578f387058627b512cc3',
+};
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
