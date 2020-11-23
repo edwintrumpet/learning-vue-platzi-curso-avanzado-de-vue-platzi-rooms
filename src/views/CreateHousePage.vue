fab0f408c59438781b8c0e963eb95dba4d7f412a
Edwin Garcia <spark.com.co@gmail.com>
2020-11-23 08:36:38 -0500

    Perfeccionando el flujo de navegacion de nuestra app

--- src/views/CreateHousePage.vue
+++ src/views/CreateHousePage.vue
@@ -90,7 +90,9 @@ export default {
         publishedAt: Date.now(),
       };
 
-      this.$store.dispatch('CREATE_ROOM', room);
+      this.$store.dispatch('CREATE_ROOM', room).then(() => {
+        this.$router.push({ name: 'SearchPage' });
+      });
     },
   },
 };
