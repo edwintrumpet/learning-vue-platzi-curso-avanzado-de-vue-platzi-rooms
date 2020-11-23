bd0eb0fb378416120ee833a2d0ed57743eed17e7
Edwin Garcia <spark.com.co@gmail.com>
2020-11-22 20:51:07 -0500

    Creacion de un componente slider utilizando la libreria tiny-slider

---
+++ src/components/TinySlider.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="slider-wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { tns } from 'tiny-slider/src/tiny-slider';
+import 'tiny-slider/dist/tiny-slider.css';
+
+export default {
+  name: 'TinySlider',
+
+  data() {
+    return {
+      $slider: null,
+    };
+  },
+
+  methods: {
+    init() {
+      const settings = {
+        container: this.$el,
+        mode: 'gallery',
+      };
+      this.$slider = tns(settings);
+    },
+  },
+
+  mounted() {
+    this.init();
+  },
+
+  beforeDestroy() {
+    if (this.$slider) {
+      this.$slider.destroy();
+    }
+  },
+};
+</script>
