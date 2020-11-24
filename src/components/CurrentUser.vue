efa67d1395f03bff67eef7468be1f34de05b5548
Edwin Garcia <spark.com.co@gmail.com>
2020-11-23 22:00:42 -0500

    Scoped slots

---
+++ src/components/CurrentUser.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="current-user">
+    <slot :user="user"></slot>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'CurrentUser',
+
+  computed: {
+    ...mapGetters({
+      user: 'authUser',
+    }),
+  },
+};
+</script>
--- src/partials/HeaderPartial.vue
+++ src/partials/HeaderPartial.vue
@@ -9,43 +9,47 @@
           >Platzi Rooms</router-link>
         </div>
         <div class="flex items-center w-auto">
-          <div class="items__controls">
-            <div class="flex" v-if="user">
-              <router-link
-                :to="{name: 'CreateHousePage'}"
-                class="no-underline mr-2 flex items-center text-grey-darkest"
-              >
-                <i class="material-icons">add</i>
-              </router-link>
-              <button class="mr-4 flex items-center">
-                <i class="material-icons">notifications</i>
-              </button>
-              <div class="flex items-center">
-                <img class="w-8 h-8 rounded-full mr-2" src="https://avatars2.githubusercontent.com/u/1901273?s=460&v=4" alt="Avatar of Javier Diaz">
-                <div class="text-sm">
-                  <p class="text-black leading-none">{{user.name}}</p>
-                  <p class="text-grey-dark">Online</p>
+          <current-user>
+            <template slot-scope="{user}">
+              <div class="items__controls">
+                <div class="flex" v-if="user">
+                  <router-link
+                    :to="{name: 'CreateHousePage'}"
+                    class="no-underline mr-2 flex items-center text-grey-darkest"
+                  >
+                    <i class="material-icons">add</i>
+                  </router-link>
+                  <button class="mr-4 flex items-center">
+                    <i class="material-icons">notifications</i>
+                  </button>
+                  <div class="flex items-center">
+                    <img class="w-8 h-8 rounded-full mr-2" src="https://avatars2.githubusercontent.com/u/1901273?s=460&v=4" alt="Avatar of Javier Diaz">
+                    <div class="text-sm">
+                      <p class="text-black leading-none">{{user.name}}</p>
+                      <p class="text-grey-dark">Online</p>
+                    </div>
+                  </div>
+                  <button class="flex items-center" @click.prevent="logOut">
+                    <i class="material-icons">exit_to_app</i>
+                  </button>
+                </div>
+                <div v-else>
+                  <button
+                    @click.prevent="getLogin"
+                    class="btn__outline btn__outline--teal rounded mr-2"
+                  >
+                    Login
+                  </button>
+                  <button
+                    @click.prevent="signUp"
+                    class="bg-yellow-dark text-yellow-darker font-semibold py-2 px-4 rounded"
+                  >
+                    Register
+                  </button>
                 </div>
               </div>
-              <button class="flex items-center" @click.prevent="logOut">
-                <i class="material-icons">exit_to_app</i>
-              </button>
-            </div>
-            <div v-else>
-              <button
-                @click.prevent="getLogin"
-                class="btn__outline btn__outline--teal rounded mr-2"
-              >
-                Login
-              </button>
-              <button
-                @click.prevent="signUp"
-                class="bg-yellow-dark text-yellow-darker font-semibold py-2 px-4 rounded"
-              >
-                Register
-              </button>
-            </div>
-          </div>
+            </template>
+          </current-user>
         </div>
       </div>
     </div>
@@ -53,15 +57,13 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import CurrentUser from '@/components/CurrentUser.vue';
 
 export default {
   name: 'HeaderPartial',
 
-  data() {
-    return {
-      isAuthenticated: true,
-    };
+  components: {
+    CurrentUser,
   },
 
   methods: {
@@ -80,12 +82,6 @@ export default {
     logOut() {
       this.$store.dispatch('LOG_OUT');
     },
-  },
-
-  computed: {
-    ...mapGetters({
-      user: 'authUser',
-    }),
   },
 };
 </script>
